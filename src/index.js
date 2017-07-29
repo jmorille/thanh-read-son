6fb149da9660051c9c7b4896180d8cc5f982304b
jmorille <jmorille@gmail.com>
2017-07-29 21:52:34 +0200

    Read all file in directory data

--- src/index.js
+++ src/index.js
@@ -1,33 +1,49 @@
 const fs = require('fs');
 const path = require('path');
-const data = require('./T01.json');
 
-
-
+//Config
 const dataDirectory = path.join(__dirname, '..', 'data');
-// console.log('dataDirectory', dataDirectory);
+const logDirectory = path.join(__dirname, '..', 'output');
 
-// const data = fs.readFileSync(path.join(dataDirectory, 'T01.json'));
-// const data = fs.readFileSync( './T01.json');
 
-var logger = fs.createWriteStream(path.join(dataDirectory, 'myFile.json'), {
+// Write File
+const logger = fs.createWriteStream(path.join(logDirectory, 'myFile.txt'), {
     flags: 'a' // 'a' means appending (old data will be preserved)
-})
-
-console.log('data', data);
-
-
-
-data.map(line => {
-    // console.log('line : ', Object.keys(line));
-    console.log('Read  : ', line.fullName, '(', line.email, ')');
-    console.log('customData : ', line.customData);
-    const cstData = line.customData;
-    const line1 = 'pom_prod_roles,'+ cstData.pom_prod_roles.join(',');
-    const line2 = 'psyco_prod_roles,'+cstData.psyco_prod_roles.join(',');
-    const line3 = 'tags_prod_roles,'+cstData.tags_prod_roles.join(',');
-    logger.write(line1+'\n');
-    logger.write(line2+'\n');
-    logger.write(line3+'\n');
 });
+
+// Read Json File
+function readJsonFile(data) {
+    return data.map(line => {
+            // console.log('line : ', Object.keys(line));
+            console.log('* Read  : ', line.fullName, '(', line.email, ')');
+    // console.log('customData : ', line.customData);
+    const cstData = line.customData;
+    const line1 = 'pom_prod_roles,' + cstData.pom_prod_roles.join(',');
+    const line2 = 'psyco_prod_roles,' + cstData.psyco_prod_roles.join(',');
+    const line3 = 'tags_prod_roles,' + cstData.tags_prod_roles.join(',');
+    logger.write(line1 + '\n');
+    logger.write(line2 + '\n');
+    logger.write(line3 + '\n');
+    // Ne sert à rien dans ton cas, juste renvoie les nouvelles valeur dans la variable results
+    return {
+        pom: cstData.pom_prod_roles.join(','),
+        psyco: +cstData.psyco_prod_roles.join(','),
+        tags: cstData.tags_prod_roles.join(',')
+    };
+})
+    ;
+}
+
+// List directory file
+const dataFiles = fs.readdirSync(dataDirectory);
+
+dataFiles.forEach(file => {
+    console.log('------- ', file, '-----------');
+   // Open File
+    const data = JSON.parse(fs.readFileSync(path.join(dataDirectory, file), 'utf8'));
+   // Parse File
+    const results = readJsonFile(data);
+    console.log(results);
+});
+
 
