830efc30bf4f1d11a8a51115ef2cd35a6abdba9e
jmorille <jmorille@gmail.com>
2017-07-29 21:16:22 +0200

    first read

--- src/index.js
+++ src/index.js
@@ -13,6 +13,11 @@ console.log('dataDirectory', dataDirectory);
 // console.log('data', data);
 data.map(line => {
     // console.log('line : ', Object.keys(line));
-    console.log('customData : ', line.customData);
+    //console.log('customData : ', line.customData);
+    const cstData = line.customData;
+    console.log('customData', cstData);
+    console.log('pom_prod_roles', cstData.pom_prod_roles);
+    console.log('psyco_prod_roles', cstData.psyco_prod_roles);
+    console.log('psyco_prod_roles', cstData.psyco_prod_roles);
 });
 
