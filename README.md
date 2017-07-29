f911c8db90507ea8dc9f2e30591813f259b803da
jmorille <jmorille@gmail.com>
2017-07-29 21:55:54 +0200

    Read Me

---
+++ README.md
@@ -0,0 +1,12 @@
+# Lecture de fichier JSON pour Thanh
+
+# Installation
+Installer la version 8 de nodejs sur https://nodejs.org/en/
+```bash
+npm i
+```
+
+# Excécution
+```bash
+npm run dev
+```
--- src/index.js
+++ src/index.js
@@ -14,34 +14,34 @@ const logger = fs.createWriteStream(path.join(logDirectory, 'myFile.txt'), {
 // Read Json File
 function readJsonFile(data) {
     return data.map(line => {
-            // console.log('line : ', Object.keys(line));
-            console.log('* Read  : ', line.fullName, '(', line.email, ')');
-    // console.log('customData : ', line.customData);
-    const cstData = line.customData;
-    const line1 = 'pom_prod_roles,' + cstData.pom_prod_roles.join(',');
-    const line2 = 'psyco_prod_roles,' + cstData.psyco_prod_roles.join(',');
-    const line3 = 'tags_prod_roles,' + cstData.tags_prod_roles.join(',');
-    logger.write(line1 + '\n');
-    logger.write(line2 + '\n');
-    logger.write(line3 + '\n');
-    // Ne sert à rien dans ton cas, juste renvoie les nouvelles valeur dans la variable results
-    return {
-        pom: cstData.pom_prod_roles.join(','),
-        psyco: +cstData.psyco_prod_roles.join(','),
-        tags: cstData.tags_prod_roles.join(',')
-    };
-})
-    ;
+        // console.log('line : ', Object.keys(line));
+        console.log('* Read  : ', line.fullName, '(', line.email, ')');
+        // console.log('customData : ', line.customData);
+        const cstData = line.customData;
+        const line1 = 'pom_prod_roles,' + cstData.pom_prod_roles.join(',');
+        const line2 = 'psyco_prod_roles,' + cstData.psyco_prod_roles.join(',');
+        const line3 = 'tags_prod_roles,' + cstData.tags_prod_roles.join(',');
+        logger.write(line1 + '\n');
+        logger.write(line2 + '\n');
+        logger.write(line3 + '\n');
+        // Ne sert à rien dans ton cas, juste renvoie les nouvelles valeur dans la variable results
+        return {
+            pom: cstData.pom_prod_roles.join(','),
+            psyco: +cstData.psyco_prod_roles.join(','),
+            tags: cstData.tags_prod_roles.join(',')
+        };
+    });
 }
 
 // List directory file
 const dataFiles = fs.readdirSync(dataDirectory);
 
+// Read All File in the directory Data
 dataFiles.forEach(file => {
     console.log('------- ', file, '-----------');
-   // Open File
+    // Open File
     const data = JSON.parse(fs.readFileSync(path.join(dataDirectory, file), 'utf8'));
-   // Parse File
+    // Parse File
     const results = readJsonFile(data);
     console.log(results);
 });
