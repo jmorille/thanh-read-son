3d0556602c2b6bb9d945383a274e1f2a64799dfc
jmorille <jmorille@gmail.com>
2017-07-30 17:52:47 +0200

    filter columnnames

--- src/index.js
+++ src/index.js
@@ -8,28 +8,29 @@ const logDirectory = path.join(__dirname, '..', 'output');
 
 // Write File
 
-const logger = fs.createWriteStream(path.join(logDirectory, 'myFile.txt'));
+const logger = fs.createWriteStream(path.join(logDirectory, 'dataFile.txt'));
+// Supprime les colones suivantes
+const filterCstKeys = ['id', 'href', 'createdAt', 'modifiedAt'];
 
 // Read Json File
-function readJsonFile(line) {
-        // console.log('line : ', Object.keys(line));
-        console.log('* Read  : ', line.fullName, '(', line.email, ')');
-        const identity = line.fullName + ';' + line.username;
-        // console.log('customData : ', line.customData);
-        const cstData = line.customData;
-        console.log('line : ', Object.keys(cstData));
-        const cstKeys = Object.keys(cstData);
-        const cstLines = cstKeys
-            .filter(key => {
-              return true;
-            }).map(key => {
+function readJsonFile(data) {
+    // console.log('line : ', Object.keys(line));
+    console.log('* Read  : ', data.fullName, '(', data.email, ')');
+    const identity = data.fullName + ';' + data.username;
+    // console.log('customData : ', line.customData);
+    const cstData = data.customData;
+    console.log('line : ', Object.keys(cstData));
+    const cstKeys = Object.keys(cstData);
+    const cstLines = cstKeys
+        .filter(key => !filterCstKeys.some(elt => elt === key))
+        .map(key => {
             const cstValue = cstData[key];
-            const cstLine = key + ';' + (Array.isArray(cstValue)?cstValue.join(','): cstValue);
+            const cstLine = key + ';' + (Array.isArray(cstValue) ? cstValue.join(',') : cstValue);
             return cstLine;
-        }).join( ';' );
-        logger.write(identity + ';' + cstLines + '\n');
-        // Ne sert à rien dans ton cas, juste renvoie les nouvelles valeur dans la variable results        return cstLines;
-        return cstLines;
+        }).join(';');
+    logger.write(identity + ';' + cstLines + '\n');
+    // Ne sert à rien dans ton cas, juste renvoie les nouvelles valeur dans la variable results        return cstLines;
+    return cstLines;
 }
 
 // List directory file
