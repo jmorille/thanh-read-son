63b8308fc65098a8686683dd60c756546e90bde5
jmorille <jmorille@gmail.com>
2017-07-30 17:29:00 +0200

    All file

--- src/index.js
+++ src/index.js
@@ -8,28 +8,27 @@ const logDirectory = path.join(__dirname, '..', 'output');
 
 // Write File
 
-const logger = fs.createWriteStream(path.join(logDirectory, 'myFile.txt'), {
-    flags: 'a' // 'a' means appending (old data will be preserved)
-});
+const logger = fs.createWriteStream(path.join(logDirectory, 'myFile.txt'));
 
 // Read Json File
 function readJsonFile(data) {
     return data.map(line => {
         // console.log('line : ', Object.keys(line));
         console.log('* Read  : ', line.fullName, '(', line.email, ')');
-
+        const identity = line.fullName + ';' + line.username;
         // console.log('customData : ', line.customData);
         const cstData = line.customData;
         console.log('line : ', Object.keys(cstData));
         const cstKeys = Object.keys(cstData);
-        const cstLines = cstKeys.map(key => {
+        const cstLines = cstKeys
+            .filter(key => {
+              return true;
+            }).map(key => {
             const cstValue = cstData[key];
             const cstLine = key + ';' + (Array.isArray(cstValue)?cstValue.join(','): cstValue);
             return cstLine;
-        }).map(line => {
-            logger.write(line + '\n');
-            return line;
-        });
+        }).join( ';' );
+        logger.write(identity + ';' + cstLines + '\n');
         // Ne sert à rien dans ton cas, juste renvoie les nouvelles valeur dans la variable results        return cstLines;
         return cstLines;
     });
